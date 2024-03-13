ebc7400ea0b377194090164d720d3e078c974381
Karim Houari <karim.houari@genesis.global>
2024-03-13 20:41:29 +0000

    accept commands with args + cd and pwd as inbuilt commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env::{current_dir, set_current_dir};
 use std::io;
 use std::io::{stdout, Write};
 use std::process::Command;
@@ -12,15 +13,59 @@ fn main() {
         if input == "exit" {
             break
         }
-        let child = Command::new(input)
-            .spawn();
-        match child {
-            Ok(child) => {
-                child.wait_with_output();
+        let parts: Vec<String> = input.split(" ").map(|s| s.to_string()).collect();
+        handle_command_with_args(parts);
+    }
+}
+
+fn handle_command_with_args(parts: Vec<String>) {
+    match parts.split_first() {
+        None => {
+            println!("Invalid command")
+        }
+        Some(splitAtFirst) => {
+            let (command, args) = splitAtFirst;
+            match command.as_str() {
+                "cd" => {
+                    cd(args);
+                }
+                "pwd" => {
+                    println!("{}", current_dir().unwrap().to_str().unwrap());
+                }
+                _ => {
+                    run_command(command, args);
+                }
             }
-            Err(_) => {
-                println!("Invalid command")
+        }
+    }
+}
+
+fn cd(args: &[String]) {
+    match args.first() {
+        None => {
+            println!("Invalid command")
+        }
+        Some(arg) => {
+            let new_path = current_dir().unwrap().as_path().join(arg);
+            if new_path.exists() {
+                set_current_dir(new_path);
+            } else {
+                println!("Path does not exist: {:?}", new_path);
             }
+        }
+    }
+}
+
+fn run_command(command: &String, args: &[String]) {
+    let child = Command::new(command)
+        .args(args)
+        .spawn();
+    match child {
+        Ok(child) => {
+            child.wait_with_output();
+        }
+        Err(_) => {
+            println!("Invalid command")
         }
     }
 }
